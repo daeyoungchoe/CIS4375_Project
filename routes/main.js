d2b5870d32dee9a9ba0b80cae49ac17d7607fe1a
tienle1996 <47994792+tienle1996@users.noreply.github.com>
2022-03-02 18:31:52 -0600

    Add Services Pages

--- routes/main.js
+++ routes/main.js
@@ -8,4 +8,12 @@ router.get('/', (req, res, next) => {
 
 })
 
+router.get('/service', (req, res, next) => {
+    // const data = req.context
+
+    res.render('service', req.context)
+
+})
+
+
 module.exports = router
