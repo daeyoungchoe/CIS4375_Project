c8dc6345635e14197f88208fece1931931d2e66e
tienle1996 <47994792+tienle1996@users.noreply.github.com>
2022-04-12 05:05:18 -0500

    Update

--- app/config/auth.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secret: "zeke-secret-key",
-  jwtExpiration: 3600, // 1 hour
-  jwtRefreshExpiration: 86400, // 24 hours
-  /* for test */
-  // jwtExpiration: 60,          // 1 minute
-  // jwtRefreshExpiration: 120,  // 2 minutes
-};
--- app/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  HOST: "us-cdbr-east-05.cleardb.net",
-  USER: "b556f5dbfefadd",
-  PASSWORD: "8c29f361 ",
-  DB: "heroku_2feb7823b585d12",
-  dialect: "mysql",
-};
