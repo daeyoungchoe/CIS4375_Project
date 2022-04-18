1c516d4b229eaf44e8f584db7c4c2a154e70f7a1
Andrew Choe <61814150+daeyoungchoe@users.noreply.github.com>
2022-04-17 19:24:31 -0500

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="container px-4 px-lg-5">
             <!-- Heading Row-->
             <br>
-            <h2 class="mb-4">Welcome to PhyZeke Phesidential Fitness</h2>
+            <h2 class="mb-4">Welcome to PhyZeke Presidential Fitness</h2>
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://i.ibb.co/DQQpygN/cover3.jpg" alt="..." /></div>
                 <div class="col-lg-5">
