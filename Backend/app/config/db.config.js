c7714379c9ad53f7894a4137037dcb3f2f87b035
Andrew Choe <61814150+daeyoungchoe@users.noreply.github.com>
2022-04-20 18:58:38 -0500

    New DB

--- Backend/app/config/db.config.js
+++ Backend/app/config/db.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  HOST: "database-1.cjekeeipgrla.us-east-1.rds.amazonaws.com",
+  //Duy's AWS account; new
+  HOST: "database-1.czfwbkc04bjz.us-east-1.rds.amazonaws.com",
+  //Andrew's AWS account that is running out of credit
+  //HOST: "database-1.cjekeeipgrla.us-east-1.rds.amazonaws.com",
   USER: "admin",
   PASSWORD: "autopilot",
   DB: "PhyzekeDB",
